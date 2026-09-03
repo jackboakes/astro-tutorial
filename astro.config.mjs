5bc186d1a2fae9e733ad5853ee20b6c5b8d5963d
jackboakes <jackboakes98@gmail.com>
2026-09-03 20:52:26 +1000

    Completed Unit 6 - Astro Islands which concludes the Astro tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://symphonious-bunny-a73445.netlify.app/"
+  site: "https://symphonious-bunny-a73445.netlify.app/",
+  integrations: [preact()]
 });
